a346d219d6f359f3262b9e87e7e1daf118b37dc5
bnsbns <y3kmarvin@gmail.com>
2023-09-23 05:58:12 +0800

    add and update file

--- vite-project/src/components/Fruits.jsx
+++ vite-project/src/components/Fruits.jsx
@@ -1,12 +1,36 @@
 export default function Fruits() {
-  const fruits = ["Apple", "Mango", "Banana", "Orange"];
+  // const fruits = ["Apple", "Mango", "Banana", "Orange"];
+  const fruits = [
+    {
+      name: "Apple",
+      price: "10",
+      emoji: "🍎",
+    },
+    {
+      name: "Mango",
+      price: "20",
+      emoji: "🥭",
+    },
+    {
+      name: "Orange",
+      price: "15",
+      emoji: "🍊",
+    },
+  ];
   return (
     <div>
-        <ul>
-      {fruits.map((fruit) => (
-        <li key={fruit}>{fruit}</li>
-      ))}
+      <ul>
+        {fruits.map((fruit) => (
+          <li key={fruit}>
+            {fruit.name} {fruit.emoji} {fruit.price}
+          </li>
+        ))}
       </ul>
+      {/* <ul>
+        {fruits.map((fruit) => (
+          <li key={fruit}>{fruit}</li>
+        ))}
+      </ul> */}
     </div>
   );
 }
